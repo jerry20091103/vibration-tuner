e37dfb4c36ca4c1796ae5a0d9c757aff3428b128
lightdance <ntuee@ntu.edu.tw>
2024-07-25 15:30:18 +0800

    add empty project

---
+++ code/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+int main() {
+	setup();
+	return 0;
+}
+
+void setup() {
+	// put your setup code here, to run once:
+	int result = myFunction(2, 3);
+	printf("Result: %d\n", result);
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+	return x + y;
+}
