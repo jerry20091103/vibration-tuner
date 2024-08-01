c30ae099a097b211803b776ee1a36ca454960205
lightdance <ntuee@ntu.edu.tw>
2024-08-01 19:50:07 +0800

    upgrade metronome backend

--- code/include/Metronome.h
+++ code/include/Metronome.h
@@ -9,14 +9,29 @@ public:
     Metronome();
     void toggleVibrating();
     bool getVibrating();
-    int getBPM();
-    bool updateBPM(int value);
+    uint8_t getBPM();
+    bool updateBPM(uint8_t value);
+    void setBPM(uint8_t BPM);
+    bool startVibrating();
+    bool stopVibrating();
+    void setBeatCount(uint8_t beatCount);
+    uint8_t getBeatCount();
+    uint8_t getCurrentBeat();
+    void setStrongHapticWaveform(uint8_t waveform);
+    void setWeakHapticWaveform(uint8_t waveform);
+    void setBuzzerLevel(uint8_t level);
 private:
-    const int MAX_BPM = 600;
-    const int MIN_BPM = 1;
-    int BPM;
+    const uint8_t MAX_BPM = 600;
+    const uint8_t MIN_BPM = 1;
+    uint8_t BPM;
     bool isVibrating;
-    int vibrationTaskID;
+    uint8_t vibrationTaskID;
+    uint8_t beatCount;
+    uint8_t currentBeat;
+    uint8_t buzzerLevel;
+    uint8_t strongHapticWaveform;
+    uint8_t weakHapticWaveform;
+    
     static void vibrationCallback();
 };
 extern Metronome metronome;
--- code/src/Metronome.cpp
+++ code/src/Metronome.cpp
@@ -8,6 +8,8 @@ Metronome::Metronome()
     BPM = 120;
     isVibrating = false;
     vibrationTaskID = -1;
+    beatCount = 4;
+    currentBeat = 0;
 }
 
 void Metronome::toggleVibrating()
@@ -19,12 +21,71 @@ void Metronome::toggleVibrating()
     }
     else
     {
+        currentBeat = 0;
         isVibrating = true;
-        int period = 60000000 / BPM;
+        uint8_t period = 60000000 / BPM;
         haptic.go();
         Serial.println("Vibrating");
         vibrationTaskID = taskManager.scheduleFixedRate(period, vibrationCallback, TIME_MICROS);
     }
+}
+
+bool Metronome::startVibrating()
+{
+    if (!isVibrating)
+    {
+        currentBeat = 0;
+        Serial.println("Start Vibrating");
+        isVibrating = true;
+        uint8_t period = 60000000 / BPM;
+        haptic.go();
+        Serial.println("Vibrating");
+        vibrationTaskID = taskManager.scheduleFixedRate(period, vibrationCallback, TIME_MICROS);
+        return true;
+    }
+    Serial.println("Error: Already vibrating");
+    return false;
+}
+
+bool Metronome::stopVibrating()
+{
+    if (isVibrating)
+    {
+        Serial.println("Stop Vibrating");
+        isVibrating = false;
+        taskManager.cancelTask(vibrationTaskID);
+        return true;
+    }
+    Serial.println("Error: Not vibrating");
+    return false;
+}
+
+void Metronome::setBPM(uint8_t BPM)
+{
+    if (BPM <= MAX_BPM && BPM >= MIN_BPM)
+    {
+        this->BPM = BPM;
+    }
+    if (BPM < MIN_BPM)
+    {
+        this->BPM = MIN_BPM;
+    }
+    if (BPM > MAX_BPM)
+    {
+        this->BPM = MAX_BPM;
+    }
+    if (isVibrating)
+    {
+        taskManager.cancelTask(vibrationTaskID);
+        uint8_t period = 60000000 / BPM;
+        vibrationTaskID = taskManager.scheduleFixedRate(period, vibrationCallback, TIME_MICROS);
+    }
+    Serial.println("Current BPM:" + String(BPM));
+}
+
+void Metronome::setBeatCount(uint8_t beatCount)
+{
+    this->beatCount = beatCount;
 }
 
 bool Metronome::getVibrating()
@@ -32,18 +93,18 @@ bool Metronome::getVibrating()
     return isVibrating;
 }
 
-int Metronome::getBPM()
+uint8_t Metronome::getBPM()
 {
     return BPM;
 }
 
-bool Metronome::updateBPM(int value)
+bool Metronome::updateBPM(uint8_t value)
 {
     if (BPM + value <= MAX_BPM && BPM + value >= MIN_BPM)
     {
         BPM += value;
         taskManager.cancelTask(vibrationTaskID);
-        int period = 60000000 / BPM;
+        uint8_t period = 60000000 / BPM;
         vibrationTaskID = taskManager.scheduleFixedRate(period, vibrationCallback, TIME_MICROS);
         Serial.println("Current BPM:" + String(BPM));
         return true;
@@ -52,8 +113,39 @@ bool Metronome::updateBPM(int value)
     return false;
 }
 
+
 void Metronome::vibrationCallback()
 {
+    metronome.currentBeat %= metronome.beatCount;
+    if (!metronome.currentBeat)
+    {
+        haptic.setWaveform(0, metronome.strongHapticWaveform);
+    }
+    else
+    {
+        haptic.setWaveform(0, metronome.weakHapticWaveform);
+    }
     haptic.go();
+    metronome.currentBeat++;
     Serial.println("Vibrating");
+}
+
+uint8_t Metronome::getBeatCount()
+{
+    return beatCount;
+}
+
+uint8_t Metronome::getCurrentBeat()
+{
+    return currentBeat;
+}
+
+void Metronome::setStrongHapticWaveform(uint8_t waveform)
+{
+    strongHapticWaveform = waveform;
+}
+
+void Metronome::setWeakHapticWaveform(uint8_t waveform)
+{
+    weakHapticWaveform = waveform;
 }
